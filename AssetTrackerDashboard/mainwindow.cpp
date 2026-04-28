ae51d5c86a49905052b08ad1fd0c4e48952e733a
vamsi <vamsikiran.mekala2@mail.dcu.ie>
2026-04-28 15:29:03 +0100

    Added dashboard again

--- AssetTrackerDashboard/mainwindow.cpp
+++ AssetTrackerDashboard/mainwindow.cpp
@@ -1,140 +1,138 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
-#include "QDebug" //allows us to print in the output console
-#include <QSslConfiguration> //required for HiveMQ security
+#include <QDebug>
 #include <QJsonDocument>
 #include <QJsonObject>
+#include <QCoreApplication>
+#include <QRandomGenerator>
+#include <QTimer>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    //Building the MQTT engine
+
+    m_secureSocket = nullptr;
     m_client = new QMqttClient(this);
+
+    // Basic Credentials
     m_client->setProtocolVersion(QMqttClient::MQTT_3_1_1);
     m_client->setUsername("vamsi");
     m_client->setPassword("Pointbreak27");
-    //HiveMq uses ssl so we tell qt to use it
-    QSslConfiguration sslConfig = QSslConfiguration::defaultConfiguration();
-    // Sender       = ui->btnConnect
-    // Signal       = &QPushButton::clicked
-    // Receiver     = this (the MainWindow itself)
-    // Slot         = &MainWindow::onConnectButtonClicked
-    connect(ui->btnConnect, &QPushButton::clicked,this,&MainWindow::onConnectButtonClicked);
-    //wiring the mqtt engine to built in connect button to signal the custom plot
-    connect(m_client,&QMqttClient::connected,this,&MainWindow::onMQTTConnected);
-    //wirre for the custom plot
-    connect(m_client, &QMqttClient::messageReceived, this, &MainWindow::onMessageReceived);
-    connect(m_client, &QMqttClient::errorChanged, this, [](QMqttClient::ClientError error){
-        qDebug() << "MQTT Error Occurred:" << error;
-    });
-    connect(ui->btnReset,&QPushButton::clicked,this,&MainWindow::on_btnReset_clicked);
 
-    //GRAPHS
+    connect(ui->btnConnect, &QPushButton::clicked, this, &MainWindow::onConnectButtonClicked);
+    connect(ui->btnReset, &QPushButton::clicked, this, &MainWindow::on_btnReset_clicked);
+    connect(m_client, &QMqttClient::connected, this, &MainWindow::onMQTTConnected);
+    connect(m_client, &QMqttClient::messageReceived, this, &MainWindow::onMessageReceived);
+
+    // Graph Setup
     m_timeCounter = 0;
-    //Create the lines
-    m_seriesX = new QLineSeries(); m_seriesX->setName("X-Axis");
-    m_seriesY = new QLineSeries(); m_seriesY->setName("Y-Axis");
-    m_seriesZ = new QLineSeries(); m_seriesZ->setName("Z-Axis");
-    //Create the chart and add the lines
+    m_seriesX = new QLineSeries(); m_seriesX->setName("X");
+    m_seriesY = new QLineSeries(); m_seriesY->setName("Y");
+    m_seriesZ = new QLineSeries(); m_seriesZ->setName("Z");
+
     m_chart = new QChart();
-    m_chart->addSeries(m_seriesX);
-    m_chart->addSeries(m_seriesY);
-    m_chart->addSeries(m_seriesZ);
-    m_chart->setTitle("Live Impact Telemetry");
+    m_chart->addSeries(m_seriesX); m_chart->addSeries(m_seriesY); m_chart->addSeries(m_seriesZ);
     m_chart->setTheme(QChart::ChartThemeDark);
-    //Create the X Axis (Time)
-    m_axisX = new QValueAxis();
-    m_axisX->setRange(0, 20); // Show 100 ticks of history
-    m_axisX->setTitleText("Time");
+
+    m_axisX = new QValueAxis(); m_axisX->setRange(0, 20);
+    m_axisY = new QValueAxis(); m_axisY->setRange(-5, 5);
+
     m_chart->addAxis(m_axisX, Qt::AlignBottom);
-    m_seriesX->attachAxis(m_axisX);
-    m_seriesY->attachAxis(m_axisX);
-    m_seriesZ->attachAxis(m_axisX);
-    //Create the Y Axis (G-Force)
-    m_axisY = new QValueAxis();
-    m_axisY->setRange(-5, 5); // G-forces between -5g and 5g
-    m_axisY->setTitleText("G-Force");
     m_chart->addAxis(m_axisY, Qt::AlignLeft);
-    m_seriesX->attachAxis(m_axisY);
-    m_seriesY->attachAxis(m_axisY);
-    m_seriesZ->attachAxis(m_axisY);
-    //Put the chart into the UI layout box!
+    m_seriesX->attachAxis(m_axisX); m_seriesY->attachAxis(m_axisX); m_seriesZ->attachAxis(m_axisX);
+    m_seriesX->attachAxis(m_axisY); m_seriesY->attachAxis(m_axisY); m_seriesZ->attachAxis(m_axisY);
+
     QChartView *chartView = new QChartView(m_chart);
-    chartView->setRenderHint(QPainter::Antialiasing); // Smooths the lines
+    chartView->setRenderHint(QPainter::Antialiasing);
     ui->graphLayout->addWidget(chartView);
 }
 
-MainWindow::~MainWindow()
-{
-    delete ui;
-}
+MainWindow::~MainWindow() { delete ui; }
 
-void MainWindow::onConnectButtonClicked() {
+void MainWindow::onConnectButtonClicked()
+{
     if(m_client->state() == QMqttClient::Disconnected) {
-        ui->btnConnect->setText("Connecting");
-        m_client->setHostname("40db01641ba149dc91182576560ea139.s1.eu.hivemq.cloud");
-        m_client->setPort(8883);
-        //command that reaches out to internet so we are using the envrypted ver
-        m_client->connectToHostEncrypted(QSslConfiguration::defaultConfiguration());
+        ui->btnConnect->setText("Connecting...");
+        ui->btnConnect->setEnabled(false);
+
+        QString host = "40db01641ba149dc91182576560ea139.s1.eu.hivemq.cloud";
+        quint16 port = 8883;
+
+        m_client->setHostname(host);
+        m_client->setPort(port);
+        // Force a unique ClientID so it doesn't clash with your Pi
+        m_client->setClientId("DesktopDash_" + QString::number(QRandomGenerator::global()->bounded(10000)));
+
+        if (m_secureSocket) {
+            m_secureSocket->abort();
+            m_secureSocket->deleteLater();
+        }
+
+        m_secureSocket = new QSslSocket(this);
+        m_secureSocket->setProxy(QNetworkProxy::NoProxy);
+
+        // 1. SSL Configuration (TLS 1.2 is mandatory for HiveMQ Cloud)
+        QSslConfiguration config = QSslConfiguration::defaultConfiguration();
+        config.setProtocol(QSsl::TlsV1_2);
+        config.setPeerVerifyMode(QSslSocket::VerifyNone);
+
+        m_secureSocket->setSslConfiguration(config);
+        m_secureSocket->setPeerVerifyName(host); // SNI
+
+        // 2. CONNECTION FLOW: TCP -> START ENCRYPTION -> MQTT LOGIN
+        connect(m_secureSocket, &QSslSocket::connected, this, [=]() {
+            qDebug() << ">>> TCP Connected. Starting SSL Handshake...";
+            m_secureSocket->startClientEncryption();
+        });
+
+        connect(m_secureSocket, &QSslSocket::encrypted, this, [=]() {
+            qDebug() << ">>> SUCCESS: Tunnel Encrypted! Sending MQTT Login...";
+            m_client->setTransport(m_secureSocket, QMqttClient::IODevice);
+            m_client->connectToHost();
+        });
+
+        connect(m_secureSocket, &QSslSocket::errorOccurred, this, [=]() {
+            qDebug() << "Socket Error:" << m_secureSocket->errorString();
+            ui->btnConnect->setText("Connect");
+            ui->btnConnect->setEnabled(true);
+        });
+
+        qDebug() << ">>> Opening TCP Socket...";
+        m_secureSocket->connectToHost(host, port);
+
     } else {
-    //if already connected it disconnects us from there upon clicking the button
         m_client->disconnectFromHost();
         ui->btnConnect->setText("Connect");
-        //Reset the graph
-        // m_seriesX->clear();
-        // m_seriesY->clear();
-        // m_seriesZ->clear();
-        // m_timeCounter = 0; // Reset time back to the start
-        // m_axisX->setRange(0, 20); // putting the window back to the beginning
     }
 }
 
 void MainWindow::onMQTTConnected() {
-    qDebug() << "Success: Connected to Hive MQ";
-    //asking HiveMQ to send us anything thats published to this topic
-    auto subscription = m_client->subscribe(QMqttTopicFilter("asset_tracker/#"));
-    if(!subscription) {
-        qDebug() << "Error could not subscribe to the topic";
-    } else {
-        qDebug() << "Successfully connected to the topic";
-    }
+    qDebug() << "MQTT Login Successful!";
     ui->btnConnect->setText("Disconnect");
+    ui->btnConnect->setEnabled(true);
+    m_client->subscribe(QMqttTopicFilter("asset_tracker/#"));
 }
 
 void MainWindow::onMessageReceived(const QByteArray &message, const QMqttTopicName &topic) {
-    qDebug() << "Message Received, Topic:" << topic.name() << "Payload:" << message;
-    //connect the raw text into JSON Object
-    QJsonDocument jsonDoc = QJsonDocument::fromJson(message);
-    if(!jsonDoc.isNull() && jsonDoc.isObject()) {
-        QJsonObject jsonObj = jsonDoc.object();
-        if(jsonObj.contains("x") && jsonObj.contains("y") && jsonObj.contains("z")) {
-            double x = jsonObj["x"].toDouble();
-            double y = jsonObj["y"].toDouble();
-            double z = jsonObj["z"].toDouble();
+    QJsonDocument doc = QJsonDocument::fromJson(message);
+    if(!doc.isObject()) return;
+    QJsonObject obj = doc.object();
 
-            ui->labelX->setText(QString("X: %1").arg(x));
-            ui->labelY->setText(QString("Y: %1").arg(y));
-            ui->labelZ->setText(QString("Z: %1").arg(z));
-            // --- UPDATE THE GRAPH ---
-            m_seriesX->append(m_timeCounter, x);
-            m_seriesY->append(m_timeCounter, y);
-            m_seriesZ->append(m_timeCounter, z);
-            m_chart->update();
+    double x = obj["x"].toDouble();
+    double y = obj["y"].toDouble();
+    double z = obj["z"].toDouble();
 
-            m_timeCounter++;
+    m_seriesX->append(m_timeCounter, x);
+    m_seriesY->append(m_timeCounter, y);
+    m_seriesZ->append(m_timeCounter, z);
+    m_timeCounter++;
 
-            // If the line hits the edge of the screen, scroll the X-axis forward
-            if (m_timeCounter > 20) {
-                m_axisX->setRange(m_timeCounter - 20, m_timeCounter);
-            }
-        }
-    }
+    if(m_timeCounter > 20) m_axisX->setRange(m_timeCounter - 20, m_timeCounter);
 }
 
 void MainWindow::on_btnReset_clicked() {
-    m_seriesX->clear();
-    m_seriesY->clear();
-    m_seriesZ->clear();
+    m_seriesX->clear(); m_seriesY->clear(); m_seriesZ->clear();
+    m_timeCounter = 0; m_axisX->setRange(0, 20);
 }
--- AssetTrackerDashboard/mainwindow.h
+++ AssetTrackerDashboard/mainwindow.h
@@ -2,16 +2,17 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-#include <QMqttClient> //MQTT library
+#include <QMqttClient>
 #include <QtCharts/QLineSeries>
 #include <QtCharts/QChart>
 #include <QtCharts/QChartView>
 #include <QtCharts/QValueAxis>
+#include <QSslConfiguration>
+#include <QSslSocket>
+#include <QNetworkProxy>
 
 QT_BEGIN_NAMESPACE
-namespace Ui {
-class MainWindow;
-}
+namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
@@ -21,27 +22,24 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
-//added this section
-private slots:
-    //custom function that will run when connect button is clicked
-    void onConnectButtonClicked();
-    //when mqtt is connected
-    void onMQTTConnected();
-    void onMessageReceived(const QByteArray &message, const QMqttTopicName &topic); //for the messsage and the topic
-    void on_btnReset_clicked();
 
+private slots:
+    void onConnectButtonClicked();
+    void onMQTTConnected();
+    void onMessageReceived(const QByteArray &message, const QMqttTopicName &topic);
+    void on_btnReset_clicked();
 
 private:
     Ui::MainWindow *ui;
-    //mqtt engine pointer
     QMqttClient *m_client;
-    //graph variables
+    QSslSocket *m_secureSocket;
+
     QLineSeries *m_seriesX;
     QLineSeries *m_seriesY;
     QLineSeries *m_seriesZ;
     QChart *m_chart;
     QValueAxis *m_axisX;
     QValueAxis *m_axisY;
-    int m_timeCounter; // To push the graph forward
+    int m_timeCounter;
 };
 #endif // MAINWINDOW_H
